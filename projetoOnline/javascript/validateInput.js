fbd55c8464a21de6dc3d7f7585f9ebe5485ede43
Eduardo Reis <up201303832@gcloud.fe.up.pt>
2016-05-26 22:37:39 +0100

    US02 concluido

--- projetoOnline/javascript/validateInput.js
+++ projetoOnline/javascript/validateInput.js
@@ -1,11 +1,25 @@
 $('form').on('submit',function(e){
+   var regex = new RegExp("^[a-zA-Z0-9]{5,15}$");
+   var regexPassword = new RegExp("^([a-zA-Z0-9@*#]{8,15})$");
+   $('.password-error-1').css({"display": "none"});
+   $('.password-error-2').css({"display": "none"});
+   $('.email-error').css({"display": "none"});
+   $('.username-error').css({"display": "none"});
    if($('.password').val()!=$('.confirmPassword').val()){
-       alert('Passwords don\'t match');
+       $('.password-error-1').css({"display": "inline"});
        return false;
    }
    else if($('.email').val()!=$('.confirmEmail').val()){
-       alert('Emails don\'t match');
+       $('.email-error').css({"display": "inline"});
        return false;
+   }
+   else if(!regex.test($('.username').val())) {
+     $('.username-error').css({"display": "inline"});
+     return false;
+   }
+   else if(!regexPassword.test($('.password').val())) {
+     $('.password-error-2').css({"display": "inline"});
+     return false;
    }
    else {
      var formData = $('form').serialize();
@@ -17,9 +31,11 @@ $('form').on('submit',function(e){
        success: function(response) {
          if(response == 'Username already in use')
             alert(response)
-         else
+         else {
             window.location.replace("../pages/login.html");
-         return false;
+            alert('Your account was successfully registered!');
+          }
+         return true;
        }
      });
    }
