1a72b26d308846fdad570e8bccc19ad61aac84e9
Eduardo Reis <up201303832@gcloud.fe.up.pt>
2016-05-26 18:45:41 +0100

    Inicio US02

--- projetoOnline/pages/checkRegister.php
+++ projetoOnline/pages/checkRegister.php
@@ -7,6 +7,15 @@
   if(createUser(1, 1, 1, $_POST['name'], $_POST['birthday'], $_POST['email'], $_POST['password'], $_POST['username']))
   {
     $_SESSION['username'] = $_POST['username'];
+
+    $subject = "Pawz Registration";
+    $message = "Thank you for registering in our website Pawz. \r\n
+                Below there are your login credentials in case you forget them. \r\n
+                \t  Username: " . $_POST['username'] . " \r\n
+                \t  Password: " . $_POST['password'] . " \r\n";
+
+    mail($_POST['email'], $subject, $message);
+
     header('Location: showFeedPage.php');
   }
 
